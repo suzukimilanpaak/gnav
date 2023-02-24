f0b7c29c116b14e37d4fae69cda59b42bd13d93d
Tatsuya Suzuki <tatsuya.suzuki@sage.com>
2023-02-24 19:15:06 +0000

    Select recent tags over branches

--- lib/gits.rb
+++ lib/gits.rb
@@ -3,21 +3,38 @@
 require 'git'
 require 'tty/prompt/vim'
 require 'logger'
+require 'open3'
 
 # require 'bundler/setup'
 # Bundler.require(:development)
+
+require 'pry'
 
 class TagExtractor
   attr_reader :git, :shas_names
 
   def initialize(git: nil)
-    @git = git || Git.open(Dir.pwd, log: Logger.new(STDOUT))
+    @git = git || Git.open(Dir.pwd)
   end
 
-  def recent_commits_with_tags
-    git.log(100).each_with_object({}) do |commit, sum|
-      tag_names = shas_names[commit.sha]
-      sum[commit] = tag_names if tag_names
+  # def recent_tags_with_commit
+  #   commits_names = recent_tag_names.each_with_object({}) do |tag_name, sum|
+  #     sum[tag_name] = git.object(tag_name) if git.object(tag_name).commit?
+  #   end
+  # end
+
+  def recent_tag_names
+    if head_changed?
+      tag_names = []
+      cmd = "git describe --tags $(git rev-list --tags --max-count=1000)"
+      _stdin, stdout, _stderr, _wait_thr = Open3.popen3(cmd)
+      stdout.each(sep="\n") do |line|
+        # reject rev without a tag
+        tag_names << line.chomp unless line =~ /-[\d]+-[a-z0-9]{8,}$/
+      end
+      @tag_names = tag_names
+    else
+      @tag_names
     end
   end
 
@@ -38,17 +55,31 @@ class TagExtractor
   end
 end
 
-# puts TagExtractor.new.recent_commits_with_tags.map {|k, v| v }
+class Selector
+  attr_reader :git, :extractor, :prompt
 
-git = Git.open(Dir.pwd, log: Logger.new(STDOUT))
-extractor = TagExtractor.new(git: git)
-prompt = TTY::Prompt.new
-selected = prompt.select('j: down, k: up, enter: choose') do |menu|
-  extractor.recent_commits_with_tags.each do |commit, tag_names|
-    tag_names.each do |tag_name|
-      menu.choice tag_name, -> { git.checkout(tag_name) }
+  def initialize(git: nil)
+    @git = git || Git.open(Dir.pwd)
+    @extractor = TagExtractor.new(git: git)
+    @prompt = TTY::Prompt.new
+  end
+
+  def select_tag
+    message = 'j/↓: down, k/↑: up, Enter: choose tag, Type to filter tags'
+    prompt.select(message, filter: true, per_page: 10) do |menu|
+      extractor.recent_tag_names.each do |tag_name|
+        menu.choice tag_name, -> { checkout(tag_name) }
+      end
     end
+  end
+
+  private
+
+  def checkout(tag_name)
+    git.checkout(tag_name)
+  rescue Git::GitExecuteError => e
+    puts e.message
   end
 end
 
-puts selected
+Selector.new.select_tag
