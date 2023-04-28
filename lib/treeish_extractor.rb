2577a6528009054199dcc7e35ab754970a1545bc
Tatsuya Suzuki <tatsuya.suzuki@sage.com>
2023-04-28 15:40:34 +0100

    Refactor Treeish Extractor

--- lib/treeish_extractor.rb
+++ lib/treeish_extractor.rb
@@ -11,32 +11,30 @@ class TreeishExtractor
   end
 
   def recent_tag_names
-    # TODO unlimit number of tags when arg is given
     cmd = 'git tag --sort=-committerdate'
-    # reject commits without an annotated tag
-    selecting_rule = ->(line, names) do
+    get_treeish_names(cmd) do |line, names|
       names << line.strip
     end
-    get_treeish_names(cmd, selecting_rule)
   end
 
   def recent_branch_names
-    # TODO limit number of branches when arg is not given
     cmd = 'git branch --sort=-committerdate'
-    # a current branch is prefixed with '* ' to indicate it's selected
-    selecting_rule = ->(line, names) do
-      names << line.strip.sub('* ', '') unless line =~ /HEAD detached/
+    get_treeish_names(cmd) do |line, names|
+      # a branch name with 'HEAD detached' means the current commit being worked on is detached from its HEAD
+      unless line =~ /HEAD detached/
+        # a branch name with '* ' is the current working one
+        names << line.strip.sub('* ', '')
+      end
     end
-    get_treeish_names(cmd, selecting_rule)
   end
 
   private
 
-  def get_treeish_names(cmd, selecting_rule)
+  def get_treeish_names(cmd)
     names = []
     _stdin, stdout, _stderr, _wait_thr = Open3.popen3(cmd)
     stdout.each(sep="\n") do |line|
-      selecting_rule.call(line, names)
+      yield(line, names)
     end
     names
   end
