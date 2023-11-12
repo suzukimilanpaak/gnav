c1cb3a3e51cdb20a4421925de7dc4dfb886b0afd
Tatsuya Suzuki <sin.wave808@gmail.com>
2023-11-12 00:28:45 +0000

    Distribute specs for Treeish to two spec files

--- spec/treeish/tag_spec.rb
+++ spec/treeish/tag_spec.rb
@@ -26,28 +26,3 @@ describe Treeish::Tag do
     end
   end
 end
-
-describe Treeish::Branch do
-  describe '#recents' do
-    subject(:treeish) { described_class.new }
-
-    before do
-      allow(Open3)
-        .to receive(:popen3)
-        .and_return([
-          nil,
-          StringIO.new("* master\n* (HEAD detached at 7250ef74d)\ndev\nrelease"),
-          nil,
-          nil
-        ])
-    end
-
-    it do
-      expect(treeish.recents).to eq([
-        { name: '* master', value: 'master' },
-        { name: '  dev', value: 'dev' },
-        { name: '  release', value: 'release' }
-      ])
-    end
-  end
-end
