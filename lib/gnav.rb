ac1c3f042b19e4802624ec53f2f143c8320dee47
Tatsuya Suzuki <sin.wave808@gmail.com>
2023-11-15 00:40:38 +0000

    Move logic to show version to gnav.rb

--- lib/gnav.rb
+++ lib/gnav.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
+require 'cli'
 require_relative './gnav/version'
 require_relative '../lib/git_prompt'
+
+file = File.expand_path('../../', __FILE__)
+gemspec = Gem::Specification::load("#{file}/gnav.gemspec")
+CLI.new do
+  description gemspec.description
+  version gemspec.version
+end.parse!
 
 module GNav
   def self.run
